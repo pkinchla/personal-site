92366152665625a35a1da238fe76383d79291eaf
Paul Kinchla <pkinchla@gmail.com>
2013-05-30 04:16:37 -0400

    changing paths of  hero images for production

--- pk/wp-content/themes/special/js/scripts.js
+++ pk/wp-content/themes/special/js/scripts.js
@@ -1,14 +1,14 @@
  $(document).ready(function() {
   
-        $('.home .hero').anystretch("/wp-content/themes/special/images/crane.jpg", {speed: 500});
-        $('.page-id-10 .hero').anystretch("/wp-content/themes/special/images/b-ball.jpg", {speed: 500});
-        $('.page-id-7 .hero').anystretch("/wp-content/themes/special/images/berry.jpg", {speed: 500});
-        $('.page-id-5 .hero').anystretch("/wp-content/themes/special/images/cham.jpg", {speed: 500});
-        $('.postid-127 .hero').anystretch("/wp-content/themes/special/images/fold.png", {speed: 500});
-        $('.single-post .hero').anystretch("/wp-content/themes/special/images/dead-ball.jpg", {speed: 500});
-        $('.error404 .hero').anystretch("/wp-content/themes/special/images/404.jpg", {speed: 500});
-        $('.date .hero').anystretch("/wp-content/themes/special/images/date.jpg", {speed: 500});
-        $('.category .hero').anystretch("/wp-content/themes/special/images/category.jpg", {speed: 500});
+        $('.home .hero').anystretch("/mothership/pk/wp-content/themes/special/images/crane.jpg", {speed: 500});
+        $('.page-id-10 .hero').anystretch("/mothership/pk/wp-content/themes/special/images/b-ball.jpg", {speed: 500});
+        $('.page-id-7 .hero').anystretch("/mothership/pk/wp-content/themes/special/images/berry.jpg", {speed: 500});
+        $('.page-id-5 .hero').anystretch("/mothership/pk/wp-content/themes/special/images/cham.jpg", {speed: 500});
+        $('.postid-127 .hero').anystretch("/mothership/pk/wp-content/themes/special/images/fold.png", {speed: 500});
+        $('.single-post .hero').anystretch("/mothership/pk/wp-content/themes/special/images/dead-ball.jpg", {speed: 500});
+        $('.error404 .hero').anystretch("/mothership/pk/wp-content/themes/special/images/404.jpg", {speed: 500});
+        $('.date .hero').anystretch("/mothership/pk/wp-content/themes/special/images/date.jpg", {speed: 500});
+        $('.category .hero').anystretch("/mothership/pk/wp-content/themes/special/images/category.jpg", {speed: 500});
       
 });
 
