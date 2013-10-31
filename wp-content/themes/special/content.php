3981e13bb9bd124ed173d5a2023ee2a1cd3992b4
Paul Kinchla <pkinchla@gmail.com>
2013-10-31 14:13:50 -0400

    updated linked headers in search results

--- wp-content/themes/special/content.php
+++ wp-content/themes/special/content.php
@@ -7,7 +7,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'special' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+		<h3 class="entry-title"><a class="linked-header" href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'special' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
