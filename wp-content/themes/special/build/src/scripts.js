15cac374c57ebf93c7cd2c76e1c3441077d4f01d
Paul Kinchla <pkinchla@gmail.com>
2013-11-02 10:21:11 -0400

    update to 404 page

--- wp-content/themes/special/build/src/scripts.js
+++ wp-content/themes/special/build/src/scripts.js
@@ -10,7 +10,7 @@
         $('.page-id-5 .hero').anystretch("/wp-content/themes/special/images/cham.jpg", {speed: 500});
         $('.postid-127 .hero').anystretch("/wp-content/themes/special/images/fold.png", {speed: 500});
         $('.single-post .hero').anystretch("/wp-content/themes/special/images/dead-ball.jpg", {speed: 500});
-        $('.error404 .hero').anystretch("/wp-content/themes/special/images/drac.gif", {speed: 500});
+        $('.error404 .hero').anystretch("/wp-content/themes/special/images/sticky-helmet.gif", {speed: 500});
         $('.date .hero').anystretch("/wp-content/themes/special/images/date.jpg", {speed: 500});
         $('.category .hero').anystretch("/wp-content/themes/special/images/category.jpg", {speed: 500});
       
