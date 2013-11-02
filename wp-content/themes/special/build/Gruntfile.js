499dcf5e1a79ab2ca49bb88709ca617b5b6d3930
Paul Kinchla <pkinchla@gmail.com>
2013-11-02 00:58:22 -0400

    adding basic grunt tasks for minifications and adding node_modules to git ignore

---
+++ wp-content/themes/special/build/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Task configuration.
+ 
+        cssmin: {
+                options: {
+                
+                },
+                build: {
+                        src: 'src/style.css',
+                        dest: '../style.css'
+                }
+        },            
+        uglify: {
+                options: {
+
+                },
+                build: {
+                        src: 'src/scripts.js',
+                        dest: '../js/scripts.js',
+                }
+        } 
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  // Default task.
+  grunt.registerTask('default', ['cssmin', 'uglify']);
+
+};
