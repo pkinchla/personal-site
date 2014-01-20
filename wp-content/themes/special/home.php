f181387cb10f57aef7a6bee52255cd26eb03f6f3
Paul Kinchla <pkinchla@gmail.com>
2014-01-19 20:42:26 -0500

    change up for content on home page

--- wp-content/themes/special/home.php
+++ wp-content/themes/special/home.php
@@ -10,10 +10,9 @@ get_header(); ?>
         <header class="entry-header">
           <h1 class="entry-title">Welcome!</h1>        
         </header>
-        <p class="col-half">Hello there. I am a developer / designer living and working in upstate NY. This is less a portfolio site and more a personal journal that soon will be the home to what I hope is some <a href="/blog/">meaningful writing</a>. The following are some key facts about me.</p>
-        
+        <p class="col-half">Hello there. I am a developer / designer living and working in upstate NY. This site is my personal online journal that soon will be the home to what I hope is some <a href="/blog/">meaningful writing</a>. The following are some key facts about me.</p>
         <ol class="col-half">
-          <li>I care about progressive enchancment and web standards.</li>
+          <li>I am pretty big on progressive enchancment and web standards.</li>
           <li>I grew up in the Boston area.</li>
           <li>I am a decent basketball player and terrible at baseball.</li>
           <li>I went to art school.</li>
