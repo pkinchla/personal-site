fdb38275a4249895f142ed463c883e0e00ebe750
Paul Kinchla <pkinchla@gmail.com>
2014-03-29 11:46:34 -0400

    changing from latest jquery to 1.11 for loading from cdn

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -129,7 +129,7 @@ add_action( 'wp_enqueue_scripts', 'special_scripts' );
 */
 function jquery_scripts() {
        wp_deregister_script('jquery');
-       wp_register_script('jquery', ("http://code.jquery.com/jquery-latest.min.js"),'','', true );
+       wp_register_script('jquery', ("https://code.jquery.com/jquery-1.11.0.min.js"),'','1.11.0', true );
        wp_enqueue_script('jquery');
 }
 add_action('wp_enqueue_scripts', 'jquery_scripts');
