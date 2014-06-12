2a6eab6276ce84e500292d6d75f13b7b255b9e0e
Paul Kinchla <pkinchla@gmail.com>
2014-06-11 23:01:43 -0400

    reinclusion of typekit in header

--- wp-content/themes/special/footer.php
+++ wp-content/themes/special/footer.php
@@ -26,26 +26,7 @@
 		
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
-<script type="text/javascript">
-  (function() {
-    var config = {
-      kitId: 'ltt0nnt'
-    };
-    var d = false;
-    var tk = document.createElement('script');
-    tk.src = '//use.typekit.net/' + config.kitId + '.js';
-    tk.type = 'text/javascript';
-    tk.async = 'true';
-    tk.onload = tk.onreadystatechange = function() {
-      var rs = this.readyState;
-      if (d || rs && rs != 'complete' && rs != 'loaded') return;
-      d = true;
-      try { Typekit.load(config); } catch (e) {}
-    };
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(tk, s);
-  })();
-</script>    
+    
 <?php wp_footer(); ?>
 </body>
 </html>
