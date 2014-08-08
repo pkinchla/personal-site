aabf03e701c41648206a03a4879726a9dbfa53d4
Paul Kinchla <pkinchla@gmail.com>
2014-08-08 13:47:50 -0400

    cleanup and reconfiguration for switch to sass and usage of livreload for development

--- wp-content/themes/special/build/Gruntfile.js
+++ wp-content/themes/special/build/Gruntfile.js
@@ -4,16 +4,43 @@
         pkg: grunt.file.readJSON('package.json'),
 
         // Task configuration.
- 
+
+        // Development task for watching templates and assets with live reload
+        sass: {
+              dist: {
+              files: {
+              '../style.css' : '**/*.scss'
+            }
+          }
+        },
+
+        watch: {
+          all: {
+            files: ['../*.php', '../inc/*.php', '../phpFlickr-3.1/*.php', '../js/*.js', '../images/*.{png,jpg,jpeg,gif,webp,svg}'],
+            options: {
+              livereload: true,  // Set livereload to trigger a reload upon change
+            }
+          },
+          css: {
+            files: ['**/*.scss'],
+            tasks: ['sass']
+          },
+            options: {
+            livereload: true, // Set livereload to trigger a reload upon change
+          }
+        },
+
+        // production
         cssmin: {
                 options: {
-                
                 },
                 build: {
-                        src: 'src/style.css',
+                        src:  '../style.css',
                         dest: '../style.css'
                 }
-        },            
+        },   
+ 
+                    
         concat: {
                 js: {
                         src: ['src/js/*'],
@@ -32,12 +59,18 @@
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-cssmin');  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat'); 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   
   
   // Default task.
-  grunt.registerTask('default', ['cssmin', 'concat', 'uglify' ]);
+  grunt.registerTask('default', ['watch']);
+  
+  // Production task for concatenation and minification
+  grunt.registerTask('deploy', ['cssmin', 'concat', 'uglify' ]);
 
 };
