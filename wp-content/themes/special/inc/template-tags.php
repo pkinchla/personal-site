4c6c2807bbe51eb5691ea33b4c143b693cb329d9
Paul Kinchla <pkinchla@gmail.com>
2014-09-18 10:27:43 -0400

    tweaks for byline on posts

--- wp-content/themes/special/inc/template-tags.php
+++ wp-content/themes/special/inc/template-tags.php
@@ -123,7 +123,7 @@ if ( ! function_exists( 'special_posted_on' ) ) :
  * @since Special 1.0
  */
 function special_posted_on() {
-	printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'special' ),
+	printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>  <span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'special' ),
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),
