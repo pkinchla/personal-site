9ef0b13827ec927d1b16c6eaf5e9bbb7a9f73e38
Paul Kinchla <pkinchla@gmail.com>
2014-10-14 18:19:19 -0400

    new templates for custom post type for portfolio

---
+++ wp-content/themes/special/archive-portfolio.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template name: Portfolio Listings
+ *
+ */
+
+// vars
+ $copy = get_field('copy');
+
+get_header(); ?>
+
+		<div id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
+			  <h1 class="entry-title"><?php the_title(); ?></h1>
+			   <section>
+  			 <ul class="portfolio-list">
+         <?php $loop = new WP_Query( array( 'post_type' => 'Portfolio', 'posts_per_page' => 20 ) ); ?>
+
+              <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                
+                  <?php $list_image = get_field('list_image'); 
+
+                  if( !empty($list_image) ): 
+
+                    // vars
+                    $url = $list_image['url'];
+                    $title = $list_image['title'];
+                    $alt = $list_image['alt'];
+                    $caption = $list_image['caption'];
+
+                    // image sizes
+                    $thumbnail = 'portfolio_small';
+                    $small = $list_image['sizes'][ $thumbnail ];
+
+                    $size_medium = 'portfolio_medium';
+                    $medium = $list_image['sizes'][ $size_medium ];
+
+                    $size_large = 'portfolio_large';
+                    $large = $list_image['sizes'][ $size_large ];
+
+                  endif; ?>
+                  
+                  <li class="col-third">
+                    <a class="portfolio-link" href="<?php the_permalink(); ?>">
+                      <span class="title"><?php the_title(); ?></span>  
+                      <img src="<?php echo $large; ?>"
+                        srcset="<?php echo $large; ?> 1100w, <?php echo $large; ?> 800w, <?php echo $large; ?> 550w"
+                        sizes="(min-width: 52em) 33vw, 100vw "alt="<?php echo $alt ?>" 
+                      />
+                    </a>
+                  </li>           
+              <?php endwhile; ?>
+            </ul>
+        </section>
+        <section>
+          <?php echo $copy; ?>
+        </section>
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/special/single-portfolio.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Template name: Portfolio Single Item
+ *
+ */
+
+$main_image_square = get_field('main_image_square'); 
+$square_image_optional = get_field('main_image_square_optional'); 
+$main_image_rectangle = get_field('main_image_rectangle'); 
+$copy = get_field('copy');
+
+
+  // vars
+  $url_rectangle = $main_image_rectangle['url'];
+  $title_rectangle = $main_image_rectangle['title'];
+  $alt_rectangle = $main_image_rectangle['alt'];
+  $caption_rectangle = $main_image_rectangle['caption'];
+
+  $url_square = $main_image_square['url'];
+  $title_square = $main_image_square['title'];
+  $alt_square = $main_image_square['alt'];
+  $caption_square = $main_image_square['caption'];
+
+  $url_square_opt = $square_image_optional['url'];
+  $title_square_opt = $square_image_optional['title'];
+  $alt_square_opt = $square_image_optional['alt'];
+  $caption_square_opt = $square_image_optional['caption'];
+
+  // base image sizes
+  $thumbnail = 'portfolio_small';
+  $size_medium = 'portfolio_medium';
+  $size_large = 'portfolio_large';
+
+  // image sizes square
+  $small_square = $main_image_square['sizes'][ $thumbnail ];
+  $medium_square = $main_image_square['sizes'][ $size_medium ];
+  $large_square = $main_image_square['sizes'][ $size_large ];
+
+  // image sizes square optional
+  $small_square_opt = $square_image_optional['sizes'][ $thumbnail ];
+  $medium_square_opt = $square_image_optional['sizes'][ $size_medium ];
+  $large_square_opt = $square_image_optional['sizes'][ $size_large ];
+
+  // image sizes rectangle
+  $small_rectangle = $main_image_rectangle['sizes'][ $thumbnail ];
+  $medium_rectangle = $main_image_rectangle['sizes'][ $size_medium ];
+  $large_rectangle = $main_image_rectangle['sizes'][ $size_large ];
+
+get_header(); ?>
+
+		<div id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
+			  <h1 class="entry-title"><?php the_title(); ?></h1>
+			   <section>
+          <?php if ($main_image_square) { ?>
+            <div class="col-third">
+              <p><?php echo $copy; ?></p>
+            </div>
+            <div class="col-two-third">		 
+              <figure class="portfolio-item">
+                <img src="<?php echo $small_square; ?>"
+                  srcset="<?php echo $large_square; ?> 1300w, <?php echo $medium_square; ?> 800w, <?php echo $small_square; ?> 550w"
+                  sizes="(min-width: 45.25em) 75.75vw, 88.8vw" alt="<?php echo $alt_square ?>" 
+                />
+              </figure>
+              <?php if ($square_image_optional) { ?>
+              <figure class="portfolio-item">
+                <img src="<?php  echo $small_square_opt; ?>"
+                srcset="<?php echo $large_square_opt;?> 1300w, <?php echo $medium_square_opt;?> 800w, <?php echo $small_square_opt;?> 550w"
+                sizes="(min-width: 45.25em) 75.75vw, 88.8vw" alt="<?php echo $alt_square_opt; ?>"
+              </figure>
+              <?php }
+                    else { }?>
+            </div>
+          <?php  } 
+                else { ?>
+            <div class="col-two-third">
+              <p><?php echo $copy; ?></p>
+            </div>
+            <div class="col-third">    
+              <figure class="portfolio-item">
+                <img class="iphone-exmaple" src="<?php echo $medium_rectangle; ?>"/>
+              </figure>
+            </div>    
+          <?php } ?>  
+        </section>
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+
+<?php get_footer(); ?>
