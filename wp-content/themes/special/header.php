782a22f76e85ced163c54f41a2168a630d3032c6
Paul Kinchla <pkinchla@gmail.com>
2014-12-09 11:54:57 -0500

    tweaks to logic for header hero image  and removal of hardcoded home page tagline

--- wp-content/themes/special/header.php
+++ wp-content/themes/special/header.php
@@ -49,20 +49,14 @@
   		</ul>
   	</div>
 	</header><!-- #masthead .site-header -->
-    <div class="hero"<?php 
-        if (has_post_thumbnail()) {
+    <?php 
+      if (has_post_thumbnail()) {
            $feat_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail_name');
-           echo ' data-stretch="'.$feat_image[0].'"';
+           echo '<div class="hero" data-stretch="'.$feat_image[0].'">' . "\n";
+           echo '</div>';
         }
         else {
           echo '';
         } 
-          echo '>'  . "\n";
-        if (is_front_page()) { 
-          echo '<div class="content">
-          <h2>Something <span>Clever</span></h2>
-          </div>' . "\n";
-        }
       ?>
-    </div>
   <div id="main" class="site-main">   
