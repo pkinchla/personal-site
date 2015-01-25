ce221de2aca0ec27342894aef662d3adbc269492
Paul Kinchla <pkinchla@gmail.com>
2015-01-25 00:16:07 -0500

    removed versioning from favicon

--- wp-content/themes/special/header.php
+++ wp-content/themes/special/header.php
@@ -15,7 +15,7 @@
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico?=v2" type="image/x-icon" />
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" type="image/x-icon" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
