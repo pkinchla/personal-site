b82096c5d2c620c910378cfa6fd3dbe8035a1a1d
Paul Kinchla <pkinchla@gmail.com>
2015-03-25 15:19:28 -0400

    addition of responsive-nav script

---
+++ wp-content/themes/special/build/src/js/scripts.js
@@ -0,0 +1,17 @@
+// write better
+'use strict';
+
+// responive nav (note: this adds js class to html element)
+var nav = responsiveNav(".main-navigation", {
+    animate: true,
+    transition: 284,
+    label: "",
+    insert: "after",
+    customToggle: "",
+    closeOnNavClick: false,
+    openPos: "relative",
+    navClass: "nav-collapse",
+    navActiveClass: "js-nav-active",
+    jsClass: "js",
+
+});
