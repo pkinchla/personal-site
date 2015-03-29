7856120b08f561518f78d922984b0de337d20a83
Paul Kinchla <pkinchla@gmail.com>
2015-03-28 22:44:57 -0400

    favicon fix for header

--- wp-content/themes/special/header.php
+++ wp-content/themes/special/header.php
@@ -13,7 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" type="image/x-icon" />
 <?php wp_head(); ?>
 </head>
 
