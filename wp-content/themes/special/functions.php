56c02c3ba8a7008ff1fac0f3efe0150d90561362
Paul Kinchla <pkinchla@gmail.com>
2015-03-30 15:59:31 -0400

    function for media uploader fix in production

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -184,6 +184,17 @@ function add_googleanalytics()  {
 add_action('wp_footer', 'add_googleanalytics');
 
 
+// media uploader fix for production
+function ms_image_editor_default_to_gd( $editors ) {
+	$gd_editor = 'WP_Image_Editor_GD';
+
+	$editors = array_diff( $editors, array( $gd_editor ) );
+	array_unshift( $editors, $gd_editor );
+
+	return $editors;
+}
+add_filter( 'wp_image_editors', 'ms_image_editor_default_to_gd' );
+
 /**
 * admin login stuff
 */
