b75d08440cf3187408ff308108867c13cc648b8d
Paul Kinchla <pkinchla@gmail.com>
2015-03-30 11:47:19 -0400

    add prism syntax highlighting for posts

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -110,11 +110,15 @@ function special_scripts() {
 		wp_enqueue_script( 'livereload', 'http://localhost:35729/livereload.js', '', null, true);
 	} 
 	else {
-		wp_enqueue_script( 'js-built', get_template_directory_uri() . '/js/built.min.js', array(), '1', true );
+		wp_enqueue_script( 'js-built', get_template_directory_uri() . '/js/built.min.js', array(), '', true );
 	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
+	}
+	if (is_single()) {
+		wp_enqueue_style( 'prism', get_template_directory_uri() . '/prism/prism.min.css' );
+		wp_enqueue_script( 'prism', get_template_directory_uri() .'/prism/prism.min.js', array(), '', true);
 	}
 }
 add_action( 'wp_enqueue_scripts', 'special_scripts' );
