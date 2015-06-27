4848d28a501e6c66d5e4b4ae6250e20895f2aceb
Paul Kinchla <pkinchla@gmail.com>
2015-06-27 08:38:02 -0400

    indenting cleanup for gruntfile

--- wp-content/themes/special/build/Gruntfile.js
+++ wp-content/themes/special/build/Gruntfile.js
@@ -1,76 +1,80 @@
-    module.exports = function(grunt){
+module.exports = function(grunt){
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+grunt.initConfig({
+pkg: grunt.file.readJSON('package.json'),
 
-        // Task configuration.
+// Task configuration.
 
-        // Development task for watching templates and assets with live reload
-        sass: {
-              dist: {
-              files: {
-              '../style.css' : 'src/css/style.scss'
-            }
-          }
-        },
+  // Development task for watching templates and assets with live reload
+  sass: {
+    dist: {
+      files: {
+        '../style.css' : 'src/css/style.scss'
+      }     
+    }
+  },
 
-        watch: {
-          all: {
-            files: ['../*.php', '../inc/*.php', '../phpFlickr-3.1/*.php', 'src/js/*.js', '../images/*.{png,jpg,jpeg,gif,webp,svg}'],
-            options: {
-              livereload: true,  // Set livereload to trigger a reload upon change
-            }
-          },
-          css: {
-            files: ['src/css/*.scss'],
-            tasks: ['sass']
-          },
-            options: {
-            livereload: true, // Set livereload to trigger a reload upon change
-          }
-        },
-        // production
-        criticalcss: {
-          custom: {
-            options: {
-                url: "http://dev.paulkinchla.com",
-                width: 1024,
-                height: 800,
-                outputfile: "../critical.css",
-                filename: "../style.css",
-            }
-          }
-        },
+  watch: {
+    all: {
+      files: ['../*.php', '../inc/*.php', '../phpFlickr-3.1/*.php', 'src/js/*.js', '../images/*.{png,jpg,jpeg,gif,webp,svg}'],
+      options: {
+      livereload: true,  // Set livereload to trigger a reload upon change
+      }
+    },
+    
+    css: {
+    files: ['src/css/*.scss'],
+    tasks: ['sass']
+    },
+    
+    options: {
+      livereload: true, // Set livereload to trigger a reload upon change
+    }
+  },
 
-        cssmin: {
-              compress: {
-                files: {
-                  '../style.css': [ '../style.css' ],
-                  '../critical.css': [ '../critical.css' ]
-                }
-              }
-        },   
- 
-                    
-        concat: {
-                js: {
-                        src: ['src/js/*'],
-                        dest: 'src/built.js',
-                        options: {
-                        separator: ';',
-                    }
-                }
-        },
-        uglify: {
-                dist: {
-                    files: {
-                  '../js/built.min.js': 'src/built.js',
-                  '../js/loadCSS.js': 'src/head-js/loadCSS.js',
-                  '../js/typekit.js': '../js/typekit.js'
-                }
-              }
-        },
-  });
+  // production
+  criticalcss: {
+    custom: {
+      options: {
+        url: "http://dev.paulkinchla.com",
+        width: 1300,
+        height: 900,
+        outputfile: "../critical.css",
+        filename: "../style.css",
+      }
+    }
+  },
+
+  cssmin: {
+    compress: {
+      files: {
+        '../style.css': [ '../style.css' ],
+        '../critical.css': [ '../critical.css' ]
+      }
+    }
+  },   
+
+
+  concat: {
+    js: {
+      src: ['src/js/*'],
+      dest: 'src/built.js',
+      options: {
+      separator: ';',
+      }
+    }
+  },
+
+  uglify: {
+    dist: {
+      files: {
+        '../js/built.min.js': 'src/built.js',
+        '../js/loadCSS.js': 'src/head-js/loadCSS.js',
+        '../js/typekit.js': '../js/typekit.js'
+      }
+    }
+  },
+});
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-sass');
@@ -79,7 +83,7 @@
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat'); 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.loadNpmTasks('grunt-criticalcss');
+  grunt.loadNpmTasks('grunt-criticalcss');
   
   
   // Default task.
