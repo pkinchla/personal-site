cc2b57e038e45bef84a91d9bc098a6ef665ca9a4
Paul Kinchla <pkinchla@gmail.com>
2015-09-08 21:02:31 -0400

    adjustment to author template

--- wp-content/themes/special/author.php
+++ wp-content/themes/special/author.php
@@ -7,10 +7,14 @@
 get_header(); ?>
 
   <div id="primary" class="content-area">
-    <main id="main" class="site-main site-content__blog" role="main">
+    <main id="main" class="site-main site-content__default site-content__author" role="main">
+      <figure class="wrapper">
+        <img class="site-content__default__heroimg" src="<?php echo get_template_directory_uri() ?>/images/cham.jpg" alt="metallic chameleon">
+      </figure>
       <div class="wrapper">
-        <h1 class="entry-title">Author: <?php the_author_link(); ?></h1>
-        <div class="site-content__blog__postlist">
+        <div class="site-content__default__body">
+          <h1 class="entry-title">Author: <?php the_author_link(); ?></h1>
+        
         <?php query_posts( 'posts_per_page=10' ); ?>
           <?php while ( have_posts() ) : the_post(); ?>
             <h4><a class="linked-header" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
@@ -18,7 +22,7 @@ get_header(); ?>
               <time datetime="<?php the_time('c');?>"><?php the_time('l, F jS, Y') ?></time>
             </div>
           <?php endwhile; // end of the loop. ?>
-        </div>
+
       </div>
     </main><!-- #content .site-content -->
   </div><!-- #primary .content-area -->
