b60363ac817bc622c079a1556f1234ae4458e943
Paul Kinchla <pkinchla@gmail.com>
2015-12-05 16:29:19 -0500

    update to grunt critical path task and quote syntax error fixes

--- wp-content/themes/special/build/Gruntfile.js
+++ wp-content/themes/special/build/Gruntfile.js
@@ -36,11 +36,11 @@ pkg: grunt.file.readJSON('package.json'),
   criticalcss: {
     custom: {
       options: {
-        url: "http://dev.paulkinchla.com",
-        width: 1300,
-        height: 900,
-        outputfile: "../critical.css",
-        filename: "../style.css",
+        url: 'http://dev.paulkinchla.com',
+        width: 1366,
+        height:768,
+        outputfile: '../critical.css',
+        filename: '../style.css',
       }
     }
   },
