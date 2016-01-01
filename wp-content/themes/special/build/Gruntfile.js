3fede5c38ea8cb307324ba18559aa314993c4b67
Paul Kinchla <pkinchla@gmail.com>
2016-01-01 15:38:23 -0500

    moved critical path rendering back to homepage. snow removal

--- wp-content/themes/special/build/Gruntfile.js
+++ wp-content/themes/special/build/Gruntfile.js
@@ -36,7 +36,7 @@ pkg: grunt.file.readJSON('package.json'),
   criticalcss: {
     custom: {
       options: {
-        url: 'http://dev.paulkinchla.com/about/',
+        url: 'http://dev.paulkinchla.com',
         width: 1366,
         height:768,
         outputfile: '../critical.css',
