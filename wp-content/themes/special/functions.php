9ced6f69911a3c2472f61399e23146dd4c9a2a84
Paul Kinchla <pkinchla@gmail.com>
2016-04-11 20:44:30 -0400

    moved prism styles into base and changed check for loading prism to only load for post rather than single

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -132,8 +132,7 @@ function special_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-	if (is_single()) {
-		wp_enqueue_style( 'prism', get_template_directory_uri() . '/prism/prism.min.css' );
+	if (get_post_type() === 'post') {
 		wp_enqueue_script( 'prism', get_template_directory_uri() .'/prism/prism.min.js', array(), '', true);
 	}
 }
