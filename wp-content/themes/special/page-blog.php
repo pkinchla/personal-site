c649eba1102a4ad4caf4ab3f47590481d3ad370e
Paul Kinchla <pkinchla@gmail.com>
2017-04-12 22:05:17 -0400

    addition of pagination for posts page

--- wp-content/themes/special/page-blog.php
+++ wp-content/themes/special/page-blog.php
@@ -8,20 +8,25 @@
  * @since    Timber 0.1
  */
 
-
+global $paged;
+if (!isset($paged) || !$paged){
+    $paged = 1;
+}
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+// $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 $blog_posts = array(
   'paged' => $paged,
   'post_type' => 'post',
   'posts_per_page' => 10
 );
+query_posts($blog_posts);
 
 $context['categories'] = Timber::get_terms('category');
 $context['blog_posts'] = Timber::get_posts($blog_posts);
+$context['pagination'] = Timber::get_pagination();
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
 
 $context['spook'] = get_cat_ID('spooky');
