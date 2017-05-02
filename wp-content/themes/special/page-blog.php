5541f70588828e6bdb4e79910415c4fd9e0e9223
Paul Kinchla <pkinchla@gmail.com>
2017-05-01 22:13:19 -0400

    tweaks to blog listing and 404 template

--- wp-content/themes/special/page-blog.php
+++ wp-content/themes/special/page-blog.php
@@ -8,10 +8,6 @@
  * @since    Timber 0.1
  */
 
-global $paged;
-if (!isset($paged) || !$paged){
-    $paged = 1;
-}
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
@@ -28,4 +24,3 @@ $context['blog_posts'] = Timber::get_posts($blog_posts);
 $context['pagination'] = Timber::get_pagination();
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
 
-$context['spook'] = get_cat_ID('spooky');
