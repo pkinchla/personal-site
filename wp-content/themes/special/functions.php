97515390443b272eaf630f2f94b11d47db835f64
Paul Kinchla <pkinchla@gmail.com>
2018-03-27 18:05:15 -0400

    fix for twig things

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -153,8 +153,6 @@ class StarterSite extends TimberSite {
 		add_theme_support( 'menus' );
 		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
 		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
 		parent::__construct();
 	}
 
@@ -169,7 +167,6 @@ class StarterSite extends TimberSite {
 	function add_to_twig( $twig ) {
 		/* this is where you can add your own fuctions to twig */
 		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
 	}
 
