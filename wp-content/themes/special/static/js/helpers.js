86a0b3bb62bf648601202e8246b4609d6ae1ebe0
Paul Kinchla <pkinchla@gmail.com>
2020-05-21 15:31:53 -0400

    some sensible updates

--- wp-content/themes/special/static/js/helpers.js
+++ wp-content/themes/special/static/js/helpers.js
@@ -1,24 +1,11 @@
-// check class on element
-export function hasClass(el, className) {
-  if (el.classList)
-    return el.classList.contains(className);
-  else
-    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-}
-
-// add class
-export function addClass(el, className) {
-  if (el.classList)
-    el.classList.add(className);
-  else if (!hasClass(el, className)) el.className += ' ' + className;
-}
-
-// remove class
-export function removeClass(el, className) {
-  if (el.classList)
-    el.classList.remove(className);
-  else if (hasClass(el, className)) {
-    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-    el.className=el.className.replace(reg, ' ');
+export function ready(fn) {
+  if (typeof fn !== 'function') {
+    return;
   }
-}
+
+  if(document.readyState === 'complete') {
+    return fn();
+  }
+
+  document.addEventListener('DOMContentLoaded', fn, false);
+};
--- wp-content/themes/special/static/js/scripts.js
+++ wp-content/themes/special/static/js/scripts.js
@@ -1,21 +1,7 @@
 // import HandleLinks from './HandleLinks'
 import MobileMenu from './MobileMenu';
+import { ready } from './helpers'
 import TurboLinks from 'turbolinks';
-
-var ready = function ( fn ) {
-
-  // Sanity check
-  if ( typeof fn !== 'function' ) return;
-
-  // If document is already loaded, run method
-  if ( document.readyState === 'complete'  ) {
-      return fn();
-  }
-
-  // Otherwise, wait until document is loaded
-  document.addEventListener( 'DOMContentLoaded', fn, false );
-
-};
 
 (function (document) {
 
