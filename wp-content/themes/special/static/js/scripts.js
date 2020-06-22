535e18cce1dae21eb8c5c064f31a453514b28942
Paul Kinchla <pkinchla@gmail.com>
2020-06-22 15:33:14 -0400

    serviceWorker updates

--- wp-content/themes/special/static/js/scripts.js
+++ wp-content/themes/special/static/js/scripts.js
@@ -3,16 +3,16 @@ import MobileMenu from './MobileMenu';
 import { ready } from './helpers';
 
 (function (document) {
-  ready(() => {
-    // start service worker
-    let dev_env = window.location.hostname === 'localhost';
-    // registration for worker for server side caching
-    if ('serviceWorker' in navigator && !dev_env) {
-      navigator.serviceWorker.register('/sw.js').then(function() {
-        return navigator.serviceWorker.ready;
-      });
-    }
+  // start service worker
+  let dev_env = window.location.hostname === 'localhost';
+  // registration for worker for server side caching
+  if ('serviceWorker' in navigator && !dev_env) {
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      return navigator.serviceWorker.ready;
+    });
+  }
 
+  ready(() => {
     // init MobileMenu
     new MobileMenu().init();
   });
