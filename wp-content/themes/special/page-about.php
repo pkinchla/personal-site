c8af3ffb45af3fdcd21c2c05953e6711c06f25f5
Paul Kinchla <pkinchla@gmail.com>
2020-08-02 14:43:40 -0400

    re-implementing instagram feed

--- wp-content/themes/special/page-about.php
+++ wp-content/themes/special/page-about.php
@@ -15,8 +15,7 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 
-$pictures = instagram_feed($instagram_creds);
-
-$context['pictures'] = $pictures;
+$pictures = instagram_feed('https://www.instagram.com/pkinchla/?__a=1');
+$context['pictures'] = $pictures->graphql->user->edge_owner_to_timeline_media->edges;
 
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
