42e298c7a1575f3c1dfd431da27abdb5f9538c1c
Paul Kinchla <pkinchla@gmail.com>
2021-01-23 14:26:24 -0500

    modern font loading for typeket

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -99,16 +99,7 @@ function enhance_js() {
 
 	echo '<script>'. $enhance_content .'</script>' . "\n";
 }
-add_action('wp_head', 'enhance_js'); // enqueue ehancements
-
-function typekit_js() {
-	$typekit_file = get_template_directory_uri() . '/js/typekit.js';
-	$typekit_path = wp_remote_get($typekit_file);
-	$typekit_content = wp_remote_retrieve_body($typekit_path);
-
-	echo '<script>'. $typekit_content .'</script>' . "\n";
-}
-add_action('wp_head', 'typekit_js');
+add_action('wp_head', 'enhance_js');
 
 // function for critical path css
 function critical_css() {
