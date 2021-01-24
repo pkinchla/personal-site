89910692f1f6040a125edc98f8b5265444453d8a
Paul Kinchla <pkinchla@gmail.com>
2021-01-24 16:09:39 -0500

    refactor to better use template hierarchy

--- wp-content/themes/special/front-page.php
+++ wp-content/themes/special/front-page.php
@@ -13,11 +13,10 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 
-
 $home_post = array(
   'post_type' => 'post',
   'posts_per_page' => 1
 );
 
 $context['home_post'] = Timber::get_posts($home_post);
-Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
+Timber::render('front-page.twig', $context );
--- wp-content/themes/special/home.php
+++ wp-content/themes/special/home.php
@@ -11,17 +11,10 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-
-$blog_posts = array(
-  'paged' => $paged,
-  'post_type' => 'post',
-  'posts_per_page' => 7
-);
-query_posts($blog_posts);
-
 $context['page_number'] = pageNumber($paged);
 $context['categories'] = Timber::get_terms('category');
-$context['blog_posts'] = Timber::get_posts($blog_posts);
+$context['posts'] = new Timber\PostQuery();
 $context['pagination'] = Timber::get_pagination();
-Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
+
+Timber::render('home.twig', $context );
 
