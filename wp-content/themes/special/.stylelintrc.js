3dbe3c0a6f88256f8185bf39b4e1f40f1cd1b65d
Paul Kinchla <pkinchla@gmail.com>
2021-08-26 17:15:03 -0400

    stylelint configuration

--- wp-content/themes/special/.stylelintrc.js
+++ wp-content/themes/special/.stylelintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: ["stylelint-order"],
-  extends: "stylelint-config-standard",
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
   rules: {
     "color-no-invalid-hex": true,
     "max-empty-lines": 2,
-    "unit-allowed-list": ["em", "rem", "%", "s"],
+    "unit-allowed-list": ["em", "rem", "%", "vh", "vw", "ms", "s"],
     "order/properties-alphabetical-order": true,
   },
 };
