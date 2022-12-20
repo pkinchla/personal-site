18d7e3abd08f6966c685fbe32b49904b191bad91
Paul Kinchla <pkinchla@gmail.com>
2022-12-20 10:12:13 -0500

    re-intro for service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = "v5.26:";
+var version = "v6:";
 
 var theme_path = "wp-content/themes/special/";
 
--- wp-content/themes/special/src/js/main.js
+++ wp-content/themes/special/src/js/main.js
@@ -1,12 +1,12 @@
 import fontSettings from "./fontSettings";
 import handleColorScheme from "./handleColorScheme";
 import observeHero from "./observeHero";
-// import invokeServiceWorker from "./invokeServiceWorker";
+import invokeServiceWorker from "./invokeServiceWorker";
 import "speedlify-score/speedlify-score.js"
 
 
 function scripts() {
-  return fontSettings(), handleColorScheme(), observeHero()
+  return fontSettings(), handleColorScheme(), observeHero(), invokeServiceWorker()
 }
 
 window.addEventListener("DOMContentLoaded", scripts);
