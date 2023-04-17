d602be0f118a2630a1a1e8c58bb897e69601dd02
Paul Kinchla <pkinchla@gmail.com>
2023-04-16 21:06:58 -0400

    change to text input and validation

--- wp-content/themes/special/src/js/fontSettings.js
+++ wp-content/themes/special/src/js/fontSettings.js
@@ -25,7 +25,10 @@ function fontSettings() {
       <label for="range">Bold Sans-serif</label>
       <input type="range" id="range" name="wght-bold" min="100" max="900" value=${defaultWght}>
       <label for="number" class="assistive-text">Bold Sans-serif</label>
-      <input id="number" type="number" min="100" max="900" name="wght-bold" step="100" value=${defaultWght} />
+      <span class="number">
+        <input aria-describedby="number-error" pattern="[0-9]+" id="number" pattern="" type="text" min="100" max="900" maxlength="3" name="wght-bold" value=${defaultWght} />
+        <span id="number-error" class="field-error" aria-live="assertive"></span>
+      </span>
     </span>
   `;
 
@@ -34,18 +37,40 @@ function fontSettings() {
   document.getElementById('colophon').prepend(element);
   var inputs = document.querySelectorAll('.font-settings input');
 
+  function validateInput(input) {
+    const isValid = input.validity.valid;
+    const isTextInput = input.type === 'text';
+    const fieldError = document.getElementById('number-error');
+
+    if (isValid && isTextInput) {
+      fieldError.innerText = '';
+      input.removeAttribute('aria-invalid', 'false');
+    } else {
+      fieldError.innerText = 'Please enter a number';
+      input.setAttribute('aria-invalid', 'true');
+    }
+  }
+
   for (const input of inputs) {
+    // validate on load
+    validateInput(input);
+
     input.addEventListener('input', function (e) {
+      // validate on input
+      validateInput(e.target);
+
       document.documentElement.style.setProperty(
         `--variable-${e.target.name}`,
         e.target.value
       );
+
       localStorage.setItem(
         'font_weight',
         getComputedStyle(document.documentElement).getPropertyValue(
           '--variable-wght-bold'
         )
       );
+
       inputs.forEach(
         (e) =>
           (e.value = getComputedStyle(
