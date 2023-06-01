d8e0f8344530f22bc7ac73a184bacaa4a8b01628
Paul Kinchla <pkinchla@gmail.com>
2023-05-31 21:28:25 -0400

    re-factor font weight number input

--- wp-content/themes/special/src/ts/fontSettings.ts
+++ wp-content/themes/special/src/ts/fontSettings.ts
@@ -28,7 +28,24 @@ function fontSettings() {
       <input type="range" id="range" name="wght-bold" min="100" max="900" value=${defaultWght}>
       <label for="number" class="assistive-text">Bold Sans-serif</label>
       <span class="number">
-        <input aria-describedby="number-error" pattern="[0-9]*" id="number" type="number" min="100" max="900" step="100" maxlength="3" name="wght-bold" value=${defaultWght} />
+        <button class="decrement">
+          <span class='assistive-text'>decrement font weight</span>
+          <span aria-hidden="true">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+              <path d="M88.2 38.5c6.2 0 11.2 4.9 11.2 11.1 0 6.2-5 11.2-11.2 11.3l-27.1.2-22.3.2-27 .2C5.6 61.5.6 56.6.6 50.4c0-6.2 5-11.2 11.2-11.3l27.1-.2 22.3-.2 27-.2z" />
+            </svg>
+          </span>          
+        </button>
+        <input pattern="[0-9]*" id="number" type="number" min="100" max="900" step="100" maxlength="3" name="wght-bold" value=${defaultWght} />
+        <button class="increment">
+          <span class='assistive-text'>increment font weight</span>
+          <span aria-hidden="true">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+              <path d="M88.2 38.5c6.2 0 11.2 4.9 11.2 11.1 0 6.2-5 11.2-11.2 11.3l-27.1.2-.2 27.1c-.1 6.1-5.1 11.1-11.3 11.1-6.2 0-11.1-5-11.1-11.1l.2-26.9-27 .2C5.5 61.5.5 56.6.5 50.4c0-6.2 5-11.2 11.2-11.3l27.1-.2.2-27.1C39.1 5.7 44.1.7 50.3.7c6.2 0 11.1 5 11.1 11.1l-.2 26.9 27-.2z" />
+            </svg>
+          </span>          
+        </button>
+        <span class="current-weight assistive-text" aria-live="assertive">current font weight is ${defaultWght}</span>
       </span>
     </span>
   `;
@@ -42,29 +59,56 @@ function fontSettings() {
     '.font-settings input'
   ) as unknown as HTMLInputElement[];
 
+  const buttons = document.querySelectorAll(
+    '.font-settings button'
+  ) as unknown as HTMLButtonElement[];
+
+  const numberInput = document.querySelector(
+    '.font-settings input[type="number"'
+  ) as HTMLInputElement;
+
+  for (const button of buttons) {
+    button.addEventListener('click', function (e) {
+      const target = e.currentTarget as HTMLButtonElement;
+
+      if (target.classList.contains('increment')) {
+        numberInput.stepUp();
+        updateFontWeight(numberInput.value);
+      }
+
+      if (target.classList.contains('decrement')) {
+        numberInput.stepDown();
+        updateFontWeight(numberInput.value);
+      }
+    });
+  }
+
   for (const input of inputs) {
     addEventListenerMulti(input, 'change input', function (e) {
       const target = e.target as HTMLInputElement;
-      document.documentElement.style.setProperty(
-        `--variable-${target.name}`,
-        target.value
-      );
-
-      localStorage.setItem(
-        'font_weight',
-        getComputedStyle(document.documentElement).getPropertyValue(
-          '--variable-wght-bold'
-        )
-      );
-
-      inputs.forEach(
-        (input) =>
-          (input.value = getComputedStyle(
-            document.documentElement
-          ).getPropertyValue('--variable-wght-bold'))
-      );
+      updateFontWeight(target.value);
     });
   }
+
+  const updateFontWeight = (value: string) => {
+    document.documentElement.style.setProperty('--variable-wght-bold', value);
+    localStorage.setItem('font_weight', value);
+
+    const currentWeight = document.querySelector(
+      '.current-weight'
+    ) as HTMLElement;
+
+    currentWeight.innerText = `Current font weight is ${getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--variable-wght-bold')}`;
+
+    inputs.forEach(
+      (input) =>
+        (input.value = getComputedStyle(
+          document.documentElement
+        ).getPropertyValue('--variable-wght-bold'))
+    );
+  };
 }
 
 export default fontSettings;
