e30566054318783ceecb57d4e07822752b24efce
Paul Kinchla <pkinchla@gmail.com>
2024-04-26 20:09:15 -0400

    removed unused query

--- wp-content/themes/special/home.php
+++ wp-content/themes/special/home.php
@@ -14,16 +14,5 @@ $context['post'] = $post;
 $context['page_number'] = pageNumber($paged);
 $context['categories'] = Timber::get_terms('category');
 
-$args = array(
-  'post_type' => 'post',
-  'posts_per_page' => 7
-);
-
-$context['posts'] = Timber::get_posts();
-
-
-// $context['posts'] = new Timber\PostQuery($args);
-// $context['pagination'] = Timber::get_pagination();
-
 Timber::render('home.twig', $context );
 
