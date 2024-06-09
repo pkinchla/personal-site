e3709b9beb1ca2093d84f77a321a15099804068b
Paul Kinchla <pkinchla@gmail.com>
2024-06-09 13:57:21 -0400

    switched font settings and color theme switcher to web components

---
+++ wp-content/themes/special/src/ts/colorSchemeSwitcher.ts
@@ -0,0 +1,105 @@
+import { getCookie } from './utils';
+
+export default class ColorSchemeSwitcher extends HTMLElement {
+  mediaQueryColorScheme: MediaQueryList;
+
+  constructor() {
+    super();
+
+    this.mediaQueryColorScheme = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    );
+  }
+
+  connectedCallback() {
+    this.mediaQueryColorScheme.addEventListener('change', (e) => {
+      const setColorScheme = getCookie('color_scheme');
+      if (setColorScheme === 'dark' || setColorScheme === 'light') return;
+      ColorSchemeSwitcher.handleChange(
+        ColorSchemeSwitcher.matchesDark(e.matches)
+      );
+    });
+
+    this.innerHTML = `
+      <fieldset class="color-theme-switcher sans-bold-italic">
+        <legend>Color Scheme</legend>
+        <input class="assistive-text" type="radio" id="system" name="color-scheme" value="system">
+        <label for="system">System</label>
+        <input class="assistive-text" type="radio" id="dark" name="color-scheme" value="dark">
+        <label for="dark">Dark</label>
+        <input class="assistive-text" type="radio" id="light" name="color-scheme" value="light">
+        <label for="light">Light</label>
+      </fieldset>
+    `;
+
+    const inputs = document.querySelectorAll(
+      `.color-theme-switcher input`
+    ) as unknown as HTMLInputElement[];
+
+    for (const input of inputs) {
+      const setColorScheme = getCookie('color_scheme');
+
+      if (input.value === setColorScheme) {
+        input.checked = true;
+      }
+
+      input.addEventListener('click', function (e) {
+        const target = e.target as HTMLInputElement;
+        ColorSchemeSwitcher.handleChange(target.value, true);
+      });
+    }
+  }
+
+  static handleChange(scheme: string | null, userToggle = false) {
+    const htmlEl = document.querySelector('html') as HTMLHtmlElement;
+    switch (scheme) {
+      case 'dark':
+        htmlEl.classList.add('dark-mode');
+        if (userToggle) {
+          document.cookie = this.setThemeCookie('dark');
+        }
+        break;
+      case 'light':
+        htmlEl.classList.remove('dark-mode');
+        if (userToggle) {
+          document.cookie = this.setThemeCookie('light');
+        }
+        break;
+      default:
+        this.handleChange(
+          this.matchesDark(
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+          )
+        );
+        if (userToggle) {
+          document.cookie = this.setThemeCookie('system');
+        }
+    }
+  }
+
+  static matchesDark(matchBool: boolean) {
+    return matchBool ? 'dark' : 'light';
+  }
+
+  static setThemeCookie(color_scheme: string) {
+    const one_year = 60 * 60 * 24 * 365;
+    return `color_scheme=${color_scheme}; path=/; max-age=${one_year}; samesite=strict; secure;`;
+  }
+
+  static initCookie() {
+    const initialValueColorScheme = getCookie('color_scheme');
+    if (!initialValueColorScheme) {
+      document.cookie = this.setThemeCookie('system');
+    }
+
+    this.handleChange(initialValueColorScheme);
+  }
+  static init() {
+    if (window.matchMedia('(prefers-color-scheme)').media === 'not all') {
+      return;
+    }
+
+    this.initCookie();
+    customElements.define('color-theme-switcher', ColorSchemeSwitcher);
+  }
+}
--- wp-content/themes/special/src/ts/main.ts
+++ wp-content/themes/special/src/ts/main.ts
@@ -1,18 +1,18 @@
-import fontSettings from './fontSettings';
-import handleColorScheme from './handleColorScheme';
 import observeHero from './observeHero';
 import invokeServiceWorker from './invokeServiceWorker';
 import checkJSLoaded from './checkJSLoaded';
 import observeTOC from './observeTOC';
+import ColorSchemeSwitcher from './colorSchemeSwitcher';
+import FontSettingsControl from './fontSettingsControl';
 import 'speedlify-score/speedlify-score.js';
 
 function scripts() {
   return (
     checkJSLoaded(),
-    fontSettings(),
-    handleColorScheme(),
     observeHero(),
     invokeServiceWorker(),
+    ColorSchemeSwitcher.init(),
+    FontSettingsControl.init(),
     observeTOC()
   );
 }
