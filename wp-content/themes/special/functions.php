825cbffca470ca7c2e2aea7c484390bf5cf1c78c
Paul Kinchla <pkinchla@gmail.com>
2024-09-15 14:44:24 -0400

    better cookie handling and dep updates

--- wp-content/themes/special/functions.php
+++ wp-content/themes/special/functions.php
@@ -303,7 +303,7 @@ class StarterSite extends Timber\Site  {
     $context['menu_type'] = explode('/', home_url( $_SERVER['REQUEST_URI'] ))[3];
     $context['perf_stats'] = speed_stats('http://stats.paulkinchla.me/api/urls.json');
     $context['bot_was_redirected'] = explode('/',  home_url( $_SERVER['QUERY_STRING']))[3] ?? null == 'sorry-bot';
-    $context['theme_class'] = htmlspecialchars($_COOKIE['color_scheme']) === 'dark' ? 'dark-mode' : '';
+    $context['theme_class'] = isset($_COOKIE['color_scheme']) && htmlspecialchars($_COOKIE['color_scheme']) === 'dark' ? 'dark-mode' : '';
 		
     return $context;
 	}
