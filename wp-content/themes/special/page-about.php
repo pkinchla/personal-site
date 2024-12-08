a23233ca9a6ea7a853fd14e591e5b09a6aa54f2e
Paul Kinchla <pkinchla@gmail.com>
2024-12-08 17:03:42 -0500

    added artwork section

--- wp-content/themes/special/page-about.php
+++ wp-content/themes/special/page-about.php
@@ -7,10 +7,27 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
-require get_template_directory() . '/credentials.php';
 
 $context = Timber::context();
 $post = Timber::get_post();
 $context['post'] = $post;
+$args = array(
+    'post_type'   => 'attachment',
+    'post_status' => 'inherit',
+    'posts_per_page' => -1,
+    'order' => 'ASC',
+    'meta_query'  => array(
+        array(
+            'key'     => 'image_type',
+            'value'   => 'artwork',
+        )
+    )
+);
+
+$attachmentQuery = new WP_Query($args);
+
+foreach ($attachmentQuery->posts as $attachment) {
+  $context['artwork'][] = Timber::get_post($attachment->ID);
+}
 
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
