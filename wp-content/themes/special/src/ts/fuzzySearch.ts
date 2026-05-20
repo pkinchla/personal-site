65a997fed1a7e4e886b87426f32e01d211577bdd
Paul Kinchla <pkinchla@gmail.com>
2026-05-20 15:08:45 -0400

    modern updates

--- wp-content/themes/special/src/ts/fuzzySearch.ts
+++ wp-content/themes/special/src/ts/fuzzySearch.ts
@@ -27,7 +27,7 @@ export default class FuzzySearch extends HTMLElement {
     this.innerHTML = `
       <button class="search-toggle">
         <span class="assistive-text">Open Search Dialog</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1200">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1200">
           <path d="M1131.8 987.11 743.25 664.49a378.98 378.98 0 0 0 72.223-267.52c-10.555-95.094-56.695-182.68-129.15-245.16s-165.86-95.258-261.48-91.734a379 379 0 0 0-254.01 110.74 379.02 379.02 0 0 0-110.74 254.01c-3.527 95.613 29.242 189.02 91.73 261.48s150.07 118.59 245.16 129.15a378.97 378.97 0 0 0 267.52-72.223l322.62 388.55c39.035 44.711 189.38-105.63 144.66-144.67zM219.85 657.77a309.66 309.66 0 0 1-84.754-158.56 309.7 309.7 0 0 1 17.621-178.93 309.685 309.685 0 0 1 286.11-191.168c61.246 0 121.12 18.16 172.05 52.188a309.69 309.69 0 0 1 131.685 317.91 309.68 309.68 0 0 1-84.754 158.56 310.04 310.04 0 0 1-437.96 0z"/>
           <path d="M574.02 215.86a258.4 258.4 0 0 0-64.195-28.008 261.25 261.25 0 0 0-255.41 66.562 254.1 254.1 0 0 0-40.848 53.145 23.997 23.997 0 0 0 27.113 35.109 23.99 23.99 0 0 0 14.535-11.25 206 206 0 0 1 33.14-43.066 213.14 213.14 0 0 1 96.855-55.418 213.1 213.1 0 0 1 111.58 1.114 210.5 210.5 0 0 1 52.258 22.805 24.001 24.001 0 0 0 24.974-40.993z"/>
         </svg>
@@ -37,7 +37,7 @@ export default class FuzzySearch extends HTMLElement {
           <search>    
             <label class="assistive-text" for="search">Search:</label>
             <input class="sans-regular-italic" type="text" id="search" placeholder="Search Site...">
-            <ul class="results" role="list" tabindex="-1"></ul>
+            <ul class="results" role="list" tabindex="-1" aria-live="polite" aria-atomic="true"></ul>
             <button class="dismiss-search-dialog close">
               <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100">
                 <line x1="22.5" y1="77.5" x2="77.5" y2="22.5" />
@@ -124,15 +124,16 @@ export default class FuzzySearch extends HTMLElement {
           const response = await fetch(`${this.apiEndpoint}${query}`);
           const results = await response.json();
 
+          const safeQuery = query.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
           return results.length > 0
-            ? `<em class="h4">Results for: ${query}</em>` +
+            ? `<em class="h4">Results for: ${safeQuery}</em>` +
                 results
                   .map(
                     (result: SearchResult) =>
                       `<li><a href="${result.url}">${result.title}</a></li>`
                   )
                   .join('')
-            : `<li>No results for: <strong>${query}</strong></li>`;
+            : `<li>No results for: <strong>${safeQuery}</strong></li>`;
         })
       )
       .subscribe((results) => (resultsContainer.innerHTML = results ?? ''));
